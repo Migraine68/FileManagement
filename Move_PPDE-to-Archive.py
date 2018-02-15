9a63288431fdc3e5b976eb31bd725329ee825b41
dgriffi6 <Migraine68@gmail.com>
2018-02-15 09:00:02 -0800

    fixed formatting suggestions and variable naming consistency

--- Move_PPDE-to-Archive.py
+++ Move_PPDE-to-Archive.py
@@ -6,6 +6,6 @@ Src = os.listdir('C:\\PPDE')
 pth = (r'c:\ppde')
 Dest = (r'B:\Reports\PPDE_Reports\Archive')
 
-for files in src:
+for files in Src:
     if files.endswith('.zipx'):
-        shutil.move(pth + '\\' + files,Dest)
+        shutil.move(pth + '\\' + files, Dest)
